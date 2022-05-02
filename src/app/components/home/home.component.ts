40b87151565839e32548d83bc375077170024648
Chrisle14820 <chris14820@gmail.com>
2022-05-02 09:22:17 -0500

    added icons to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faCircleUser,
+  faSquarePhone,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  faCircleUser = faCircleUser;
+  faPhone = faSquarePhone;
+  faFilePdf = faFilePdf;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
